1aab3546063ec9af7ed72175e7958ed4c4831426
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2022-11-24 13:05:48 +0300

    first app structure #1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="wrapper">
+    <ul id="messages"></ul>
+    <fotm id="form">
+      <input v-model="msg" type="text" autocomplete="off" />
+      <input type="submit" value="Send" @click="onSubmit"/>
+    </fotm>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+const socket = io();
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  created() {
+      socket.on('connect', () => {
+      console.log('Client connected');
+    });
+  },
+  methods: {
+    onSubmit() {
+      if (this.msg) {
+        socket.emit('chat message', this.msg);
+        this.msg = '';
+      };
+    },
+  },
+};
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style lang="scss">
+  @import './assets/nullstyle.scss';
 </style>
